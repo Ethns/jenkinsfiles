7a6ca402a026d3d1431d18f9e068dc3be5399bc1
Ethan Shen <ethan.shen@gmail.com>
2021-03-20 12:49:58 +0800

    archiving output files

--- sample.groovy
+++ sample.groovy
@@ -2,17 +2,20 @@
 node {
     stage "Create build output"
     
+    // show path of the current directory
+    sh "pwd"
+
     // Make the output directory.
-    // sh "mkdir -p output"
+    sh "mkdir -p output"
 
     // Write an useful file, which is needed to be archived.
-    // writeFile file: "output/usefulfile.txt", text: "This file is useful, need to archive it."
+    writeFile file: "output/usefulfile.txt", text: "This file is useful, need to archive it."
 
     // Write an useless file, which is not needed to be archived.
-    // writeFile file: "output/uselessfile.md", text: "This file is useless, no need to archive it."
+    writeFile file: "output/uselessfile.md", text: "This file is useless, no need to archive it."
 
     stage "Archive build output"
     
     // Archive the build output artifacts.
-    // archiveArtifacts artifacts: 'output/*.txt', excludes: 'output/*.md'
+    archiveArtifacts artifacts: 'output/*.txt', excludes: 'output/*.md'
 }
