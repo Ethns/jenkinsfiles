f7e80570cb85776eca61c9291cab7ea8f9653ea5
Ethan S <shenye601@hotmail.com>
2023-03-11 17:27:48 +1000

    To trigger webook event

--- tester.groovy
+++ tester.groovy
@@ -2,9 +2,11 @@ def raw = """com.key1.user=user1
 com.key1.pass=call(key1)
 com.key2.user=user2
 com.key2.pass=call(key2)"""
-print(raw)
+println(raw)
 println('\n')
 def key1 = /call(key1)/
+println(key1)
 def key2 = /call(key2)/
+println(key2)
 def parsed1 = raw.replace(key1, 'password1')
 print(parsed1)
